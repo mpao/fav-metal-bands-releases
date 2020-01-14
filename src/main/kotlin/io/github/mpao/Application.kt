612a9330476ea80028d0291fac9c987e825f5fcd
marco <mpao@me.com>
2020-01-14 18:41:09 +0100

    adds run time calculator

--- src/main/kotlin/io/github/mpao/Application.kt
+++ src/main/kotlin/io/github/mpao/Application.kt
@@ -4,12 +4,13 @@ import io.github.mpao.metal.MetalArchiveScraper
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
+import kotlin.system.measureTimeMillis
 
 fun main() {
 
     val list = listOf(
         "Nazgul::7829",
-        "draugr::14632",
+        "draugr",
         "orietta berti",
         "emyn muil",
         "atavicus",
@@ -17,13 +18,16 @@ fun main() {
     )
 
     runBlocking {
-        list.asSequence().map {
-            async(Dispatchers.IO) {
-                MetalArchiveScraper().apply { client = ProxyClient().client }.searchBand(it)
+        val time = measureTimeMillis {
+            list.map {
+                async(Dispatchers.IO) {
+                    MetalArchiveScraper().searchBand(it)
+                }
+            }.forEach {
+                println(it.await())
             }
-        }.forEach {
-            println(it.await())
         }
+        println("Completed in $time ms")
     }
 
 }
